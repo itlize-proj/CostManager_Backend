ffa0d58808283b7b089e3bdce7fc772aa95f7d85
Yohannes Habte <yohannes.habte@mnsu.edu>
2022-01-13 10:00:46 -0600

    revert to a45a175

--- GroupProject/src/test/java/com/example/groupproject/repositories/ProjectRepositoryTest.java
+++ GroupProject/src/test/java/com/example/groupproject/repositories/ProjectRepositoryTest.java
@@ -8,14 +8,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.ActiveProfiles;
+
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-@ActiveProfiles("test")
 class ProjectRepositoryTest {
     @Autowired
     private ProjectRepository projectRepository;
