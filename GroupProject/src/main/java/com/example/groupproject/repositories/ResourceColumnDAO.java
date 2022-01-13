687490e6d223fc48c31f2fec9b18db338613d31e
Yohannes Habte <yohannes.habte@mnsu.edu>
2022-01-12 18:18:43 -0600

    fix repository conflicts

--- GroupProject/src/main/java/com/example/groupproject/repositories/ResourceColumnDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.groupproject.repositories;
-
-import com.example.groupproject.entities.ResourceColumn;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ResourceColumnDAO extends JpaRepository<ResourceColumn,Integer> {
-}
--- GroupProject/src/main/java/com/example/groupproject/repositories/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.groupproject.repositories;
-
-import com.example.groupproject.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserDAO extends JpaRepository<User, Integer> {
-}
